091b5ce113f3199efd02d7f67387948ecce1bb81
krisna31 <81416312+krisna31@users.noreply.github.com>
2023-05-06 15:19:25 +0700

    perubahan mahassiswa.java

--- Mahasiswa.java
+++ Mahasiswa.java
@@ -1,10 +1,15 @@
 public class Mahasiswa {
  private String npm;
+ private String nama;
 
  public Mahsiswa() {}
 
  public getNpm() {return npm;}
 
  public setNpm(String npm) {this.npm = npm;}
+
+ public getNama() {return nama;}
+
+ public setNama(String nama) {this.nama = nama;}
 
 }
